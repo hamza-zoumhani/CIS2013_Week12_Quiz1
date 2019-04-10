3c6a6f0d48c9a86892c1a301510a42d277fddc9f
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-04-10 18:17:36 -0500

    mine_sweeper

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include "game.cpp"
+#include "mine_sweeper.cpp"
+
 
 using namespace std;
 
---
+++ mine_sweeper.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+class mine_sweeper{
+	
+	private:
+		int board_size;
+		int board_rows;
+		int board_columns;
+		int selected_squares;
+		int bomb_squares;
+		
+	
+};
